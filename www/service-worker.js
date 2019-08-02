7a74f84a98fd36e8a1e6b024e2d2f8488899481a
kaiurin <B100xybR0408>
2019-08-02 23:31:11 +0300

    - added QR pairing code

--- www/service-worker.js
+++ www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83ee2dd692fb1853af3a61100f71851c.js"
+  "/precache-manifest.c665361eb09e11b7ffaed8500056ebe9.js"
 );
 
 workbox.clientsClaim();
